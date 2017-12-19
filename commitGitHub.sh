b8a826be079bf9367069639d4173c30854ecfced
Silviu Rei <silviu.rei@gmail.com>
2017-12-19 11:25:50 +0200

    x

--- commitGitHub.sh
+++ commitGitHub.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 git add *
 git commit -m "x"
-git add origin https://github.com/SilviuRei/MatlabDLSToolkit.git
+git remote add origin https://github.com/SilviuRei/MatlabDLSToolkit.git
 git push -f origin master
